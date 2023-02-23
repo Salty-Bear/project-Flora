9137858b8e9e8d72f08b2b93c379f0feeabf7931
SuprHUlk <ayush123kumar.ak@gmail.com>
2023-02-24 03:16:17 +0530

    ok

--- project-flora/src/app/signup-page/signup-page.component.ts
+++ project-flora/src/app/signup-page/signup-page.component.ts
@@ -8,18 +8,47 @@ import { Signup } from '../signup-page/signup.model';
   styleUrls: ['./signup-page.component.css']
 })
 export class SignupPageComponent {
-  f_name=''; //first name
-  l_name =''; //last name
-  u_name =''; //user_name
-  email=''; //email
-  password=''; // password
+  f_name: string=''; //first name
+  l_name: string=''; //last name
+  u_name: string=''; //user_name
+  email: string=''; //email
+  password: string=''; // password
+  errorMessage: any = null;
   constructor(private http: HttpClient){}
-  signups : Signup =new Signup(this.f_name,this.l_name,this.u_name,this.email,this.password);
+  signup : Signup =new Signup(this.f_name,this.l_name,this.u_name,this.email,this.password);
   post(){
     console.log("ok");
-    alert("Signed in! Sucessfully")
-    this.signups=new Signup(this.f_name,this.l_name,this.u_name,this.email,this.password);
-    this.http.post('https://flora-fbf5b-default-rtdb.firebaseio.com/posts.json',this.signups).subscribe(responseData =>{console.log(responseData)});
-    this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCXDVFX6EdK1-4DpbEGrqocOgpPAEqN7DQ',{email: this.signups.email, password: this.signups.pass, returnSecureToken: true}).subscribe(error =>{console.log(error)});
+    this.signup=new Signup(this.f_name,this.l_name,this.u_name,this.email,this.password);
+    this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCXDVFX6EdK1-4DpbEGrqocOgpPAEqN7DQ',
+      {email: this.signup.email, password: this.signup.pass, returnSecureToken: true}
+    ).
+    subscribe(
+      resData => { 
+        console.log(resData);
+        alert("Signed in! Successfully");
+        this.http.post('https://flora-fbf5b-default-rtdb.firebaseio.com/posts.json',this.signup).subscribe(responseData =>{console.log(responseData)});
+      }, 
+      errorRes =>{
+        console.log(errorRes);
+        if (!errorRes.error || !errorRes.error.error.message){
+          this.errorMessage = "UNKNOWN_ERROR";
+        }
+        else {
+          switch(errorRes.error.error.message) {
+            case 'EMAIL_EXISTS': {
+              this.errorMessage = "EMAIL_EXISTS";
+              break;
+            }
+            case 'WEAK_PASSWORD : Password should be at least 6 characters': {
+              this.errorMessage = "WEAK_PASSWORD : Password should be at least 6 characters"
+              break;
+            }
+            default: {
+              this.errorMessage = "UNKNOWN_ERROR";
+            }
+          }
+        }
+      }
+    );
   }
 }
