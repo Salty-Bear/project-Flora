3859dcd09ecccb6e3301b992f31da3b2529694ff
SuprHUlk <ayush123kumar.ak@gmail.com>
2023-02-24 15:52:29 +0530

    ok

--- project-flora/src/app/signup-page/signup-page.component.ts
+++ project-flora/src/app/signup-page/signup-page.component.ts
@@ -8,16 +8,19 @@ import { Signup } from '../signup-page/signup.model';
   styleUrls: ['./signup-page.component.css']
 })
 export class SignupPageComponent {
+
   f_name: string=''; //first name
   l_name: string=''; //last name
   u_name: string=''; //user_name
   email: string=''; //email
   password: string=''; // password
   errorMessage: any = null;
+
   constructor(private http: HttpClient){}
+
   signup : Signup =new Signup(this.f_name,this.l_name,this.u_name,this.email,this.password);
+  
   post(){
-    console.log("ok");
     this.signup=new Signup(this.f_name,this.l_name,this.u_name,this.email,this.password);
     this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCXDVFX6EdK1-4DpbEGrqocOgpPAEqN7DQ',
       {email: this.signup.email, password: this.signup.pass, returnSecureToken: true}
