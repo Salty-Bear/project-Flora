0a992205ab1396d4c1cbc24e313b67f73bf37172
SuprHUlk <ayush123kumar.ak@gmail.com>
2023-03-03 22:12:41 +0530

    ok

--- project-flora/src/app/app.module.ts
+++ project-flora/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from "@angular/router";
 
