1f04cf7bfc62b5074129106c0a6e8098fa449001
Aryaman Raj <aryamanraj123456789@gmail.com>
2023-03-15 22:03:27 +0530

    ok

---
+++ project-flora/src/app/main/home/letter/letter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-letter',
+  templateUrl: './letter.component.html',
+  styleUrls: ['./letter.component.css']
+})
+export class LetterComponent {
+
+}
