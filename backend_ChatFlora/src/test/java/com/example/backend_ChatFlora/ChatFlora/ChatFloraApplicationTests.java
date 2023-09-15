827cdd03e547e4c203a139346bb7a3730a544412
SuprHUlk <ayush123kumar.ak@gmail.com>
2023-09-16 02:38:24 +0530

    Connected to DB

--- backend_ChatFlora/src/test/java/com/example/backend_ChatFlora/ChatFlora/ChatFloraApplicationTests.java
+++ backend_ChatFlora/src/test/java/com/example/backend_ChatFlora/ChatFlora/ChatFloraApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backend.chatflora;
+package com.example.backend_ChatFlora.ChatFlora;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChatfloraApplicationTests {
+class ChatFloraApplicationTests {
 
 	@Test
 	void contextLoads() {
